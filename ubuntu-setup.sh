00fb2502473b9ebc98f5573c4c88698e02e71d90
Dima <dima@current.ai>
2023-12-25 11:48:59 +0800

    Added `sudo` check.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -6,25 +6,33 @@
 
 # TODO tomorrow: Add `../current` and `../../current` into the YCM header path.
 
-sudo apt-get -y update
-sudo apt-get install -y git
+if ! ( [ "$EUID" -eq 0 ] || SUDO_ASKPASS=/bin/false sudo -A /bin/true >/dev/null 2>&1) ; then
 
-git clone https://github.com/dimacurrentai/dotfiles ~/.dotfiles
+  echo 'Need `sudo`, or run as `root.'
 
-ALL_APT_PACKAGES=""
-for i in $(cat .dotfiles/apt-packages.txt); do ALL_APT_PACKAGES="$ALL_APT_PACKAGES $i"; done
+else
 
-time sudo apt-get install -y $ALL_APT_PACKAGES
+  sudo apt-get -y update
+  sudo apt-get install -y git
 
-sudo mkdir -p /usr/lib/android-sdk/licenses
-sudo chmod a+rw  /usr/lib/android-sdk/licenses
-sudo mkdir -p /opt/android-sdk
-sudo chmod a+rw  /opt/android-sdk
-yes | sdkmanager --licenses
+  git clone https://github.com/dimacurrentai/dotfiles ~/.dotfiles
 
-cp $(find .dotfiles/ -maxdepth 1 -name '.*' -type f) .
+  ALL_APT_PACKAGES=""
+  for i in $(cat .dotfiles/apt-packages.txt); do ALL_APT_PACKAGES="$ALL_APT_PACKAGES $i"; done
 
-sudo chsh -s $(which zsh) $(whoami)
+  time sudo apt-get install -y $ALL_APT_PACKAGES
 
-git clone --depth 1 --recurse-submodules --shallow-submodules https://github.com/Valloric/YouCompleteMe ~/.vim/pack/plugins/opt/YouCompleteMe
-(cd ~/.vim/pack/plugins/opt/YouCompleteMe; ./install.py --all)
+  sudo mkdir -p /usr/lib/android-sdk/licenses
+  sudo chmod a+rw  /usr/lib/android-sdk/licenses
+  sudo mkdir -p /opt/android-sdk
+  sudo chmod a+rw  /opt/android-sdk
+  yes | sdkmanager --licenses
+
+  cp $(find .dotfiles/ -maxdepth 1 -name '.*' -type f) .
+
+  sudo chsh -s $(which zsh) $(whoami)
+
+  git clone --depth 1 --recurse-submodules --shallow-submodules https://github.com/Valloric/YouCompleteMe ~/.vim/pack/plugins/opt/YouCompleteMe
+  (cd ~/.vim/pack/plugins/opt/YouCompleteMe; ./install.py --all)
+
+fi

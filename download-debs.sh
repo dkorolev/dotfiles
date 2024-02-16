4ed4a1acc1611f8debc0cea95c2c1eb154372a65
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-02-16 17:14:39 +0100

    staying safe for the test

--- download-debs.sh
+++ download-debs.sh
@@ -6,8 +6,8 @@ ALL_APT_PACKAGES=””
 
 for i in $(cat ${SCRIPT_DIR}/apt-packages.txt | grep -v '^#'); do ALL_APT_PACKAGES="$ALL_APT_PACKAGES $i"; done
 
-#DEBS=$(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends ${ALL_APT_PACKAGES} | grep "^\w")
-DEBS=$(apt-cache depends --recurse ${ALL_APT_PACKAGES} | grep "^\w")
+# Ref. https://unix.stackexchange.com/questions/367942/make-apt-cache-depends-only-recurse-over-preferred-alternative
+DEBS=$(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends -o APT::Cache::ShowOnlyFirstOr=true ${ALL_APT_PACKAGES} | grep "^\w")
 
 mkdir -p deb
 for i in $DEPS ; do

986efe1a21aaa7e4cc4aa0a8abc89bf7078b2fad
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-02-17 12:00:02 +0100

    Cleaning up `ubuntu-setup.sh`.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# This "script" is meant to be copy-pasted into a fresh Ubuntu.
+# This "script" is meant to be run on a fresh Ubuntu installation.
 #
-# Testing under WSL, because why not?
+# Tested under WSL too, except, of course, `chromium`.
 
 if ! ( [ "$EUID" -eq 0 ] || SUDO_ASKPASS=/bin/false sudo -A /bin/true >/dev/null 2>&1) ; then
 
@@ -15,7 +15,8 @@ else
   sudo apt-get -y update
   sudo apt-get install -y git
 
-  git clone https://github.com/dkorolev/dotfiles ~/.dotfiles
+  # TODO(dkorolev): The `Makefile` and a self-contained copy-pasteable command.
+  # git clone https://github.com/dkorolev/dotfiles ~/.dotfiles
 
   ALL_APT_PACKAGES=""
   # TODO(dkorolev): have `#` comment until the EOL, not only if it's the first char of the line!
@@ -29,15 +30,21 @@ else
   echo "APT packages installation took $((T_APT_DONE-T_BEGIN))s."
   echo
 
+  # TODO(dkorolev): This did not work on an ARM.
   sudo mkdir -p /usr/lib/android-sdk/licenses
   sudo chmod a+rw  /usr/lib/android-sdk/licenses
   sudo mkdir -p /opt/android-sdk
   sudo chmod a+rw  /opt/android-sdk
   yes | sdkmanager --licenses
 
+  # Install the dotfiles.
   cp $(find .dotfiles/ -maxdepth 1 -name '.*' -type f) .
 
+  # Set the shell to `zsh`.
   sudo chsh -s $(which zsh) $(whoami)
+
+  # Also, copy the dotfiles into `root`.
+  for i in $(find .dotfiles/ -maxdepth 1 -name '.*' -type f) ; do sudo cp $i /root ; sudo chown root: /root/$i ; done
 
   git clone --depth 1 --recurse-submodules --shallow-submodules https://github.com/Valloric/YouCompleteMe ~/.vim/pack/plugins/opt/YouCompleteMe
 

0ee39ef36a7c9276ae9945c0ac49b4ec60975cd5
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-02-18 21:16:26 +0100

    More chmods I said, again.

--- pack_user.sh
+++ pack_user.sh
@@ -84,7 +84,7 @@ fi
 
 sudo mkdir -p "/var/userdata/scripts/$U"
 sudo chown $U: "/var/userdata/scripts/$U"
-sudo a+x "/var/userdata/scripts/$U"
+sudo chmod a+x "/var/userdata/scripts/$U"
 sudo rm -f "/var/userdata/scripts/$U/doit.sh"
 sudo touch "/var/userdata/scripts/$U/doit.sh"
 sudo chown $U: "/var/userdata/scripts/$U/doit.sh"

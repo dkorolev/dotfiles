6780d31dc1c37fce45e1a8c30e6339a826e58634
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-02-18 13:10:34 +0100

    Started `dkorolev_pack_user.sh`.

---
+++ chrome_default_profile_base_dir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# != 1 ] ; then
+  echo 'Need one arg, the user name.'
+  exit 1
+fi
+
+U="$1"
+D="/home/$U/snap/chromium/common/chromium/Default"
+
+if [ -d "$D" ] ; then
+  echo "$(dirname "$D")"
+  exit 0
+else
+  exit 1
+fi
---
+++ dkorolev_pack_user.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Must be run as root while the user is logged in so that their directory is not encrypted.
+
+set -e
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+U=$1
+
+if [ "$U" == "" ] ; then
+  echo 'Need one argument: the username.'
+  exit 1
+fi
+
+if [[ $UID == 0 || $EUID == 0 ]] ; then
+  echo 'Please do not run as `root`.'
+  exit 1
+elif ! ( [ "$EUID" -eq 0 ] || SUDO_ASKPASS=/bin/false sudo -A /bin/true >/dev/null 2>&1) ; then
+  echo 'Need `sudo`.'
+  exit 1
+fi
+
+if sudo [ -f /home/dima/Access-Your-Private-Data.desktop ] ; then
+  echo 'Need to log in as that user since their homedir is encrypted.'
+  exit 1
+fi
+
+"${SCRIPT_DIR}/chrome_default_profile_base_dir.sh"
+
+sudo cat "/var/lib/AccountsService/users/$U" | grep '^Icon=' | sed "s/^Icon=//"
+
+sudo runuser -u "$U" -- gsettings get org.gnome.desktop.background picture-uri  | xargs echo | sed 's/^file:\/\///'

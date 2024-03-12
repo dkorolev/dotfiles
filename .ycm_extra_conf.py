39d0b2b52f18428a14f4ac8a1ffc0d00a89c818f
Dima <dima@current.ai>
2024-03-12 15:31:56 +0100

    Updated `.ycm_extra_conf.py`.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -10,6 +10,10 @@ def Settings(**kwargs):
         '-I', './current/',
         '-I', '../current/',
         '-I', '../../current/',
+        # For `cmake`-based builds of C++ projects using `C5T/Current`.
+        '-DC5T_CMAKE_PROJECT',
+        '-I', '.current/inc',
+        '-I', '.current_debug/inc',
         # For `#include <gtest/test.h>`, when using `googletest` via `CMakeLists.txt`.
         '-isystem', './googletest/googletest/include',
         '-isystem', '../googletest/googletest/include',

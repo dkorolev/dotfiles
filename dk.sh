c3546a9bb353a32cfeca257af9a2953b0e858ce5
Dima Korolev <dima@current.ai>
2026-02-13 11:44:45 +0000

    Mounting `.dk.tmp` as `/tmp`.

--- dk.sh
+++ dk.sh
@@ -3,12 +3,15 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 
+mkdir -p "$SCRIPT_DIR/.dk.tmp"
+
 if ! (cd "$SCRIPT_DIR/docker/dk"; make -q) ; then
   echo 'Rebuilding the container.'
   (cd "$SCRIPT_DIR/docker/dk"; make >/dev/null)
   echo 'Rebuilt successful.'
 fi
 docker run -it --hostname dk \
+  -v "$SCRIPT_DIR/.dk.tmp:/tmp" \
   -e DK_UID="$(id -u)" \
   -e DK_GID="$(id -g)" \
   -e DK_USER="$(whoami)" \
